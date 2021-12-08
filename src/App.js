3c606d66342486edebf996a18665118318f98bb4
novailoveyou <novailoveyoux@gmail.com>
2021-12-08 23:08:02 +0300

    dynaic values & lists in jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 function App() {
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {
+      id: 1,
+      text: 'Comment one'
+    },
+    {
+      id: 2,
+      text: 'Comment two'
+    },
+    {
+      id: 3,
+      text: 'Comment three'
+    }
+  ]
+
   return (
     <div className='container'>
-      <h1>My App</h1>
-      <p>Hello</p>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, idx) => (
+            <li key={comment.id}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
