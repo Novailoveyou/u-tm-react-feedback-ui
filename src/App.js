9b4f7bd43433f099d4d290bf5a90e74d543ba130
novailoveyou <novailoveyoux@gmail.com>
2022-07-03 01:48:34 +0300

    state & useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
 
 const App = () => {
   return (
     <>
       <Header />
       <div className='container'>
-        <h1>My App</h1>
+        <FeedbackItem />
       </div>
     </>
   )
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+const FeedbackItem = () => {
+  const [rating, setRating] = useState(7)
+  const [text, setText] = useState('This is an example of a feedback item')
+
+  const handleClick = () => {
+    setRating(prev => {
+      return prev + 1
+    })
+  }
+
+  return (
+    <div className='card'>
+      <div className='num-display'>{rating}</div>
+      <div className='text-display'>{text}</div>
+      <button onClick={handleClick}>Click</button>
+    </div>
+  )
+}
+
+export default FeedbackItem
