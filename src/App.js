8e6101e3933daaec63d2272dd056313a3fc1e8c0
novailoveyou <novailoveyoux@gmail.com>
2022-07-03 01:00:27 +0300

    creating first component & props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='container'>
-      <h1>My App</h1>
-    </div>
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
   )
 }
 
