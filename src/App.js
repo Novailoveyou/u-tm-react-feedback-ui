0f8ccf1e38b19af0c3f2d6644a9fe167f9605230
novailoveyou <novailoveyoux@gmail.com>
2022-07-02 22:53:05 +0300

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
 import React from 'react'
 
 const App = () => {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const comments = [
-    {
-      id: 1,
-      text: 'Comment one'
-    },
-    {
-      id: 2,
-      text: 'Comment two'
-    },
-    {
-      id: 3,
-      text: 'Comment three'
-    }
-  ]
-
-  const loading = false
-  const showComments = true
-
-  if (loading) return <h1>Loading...</h1>
-
-  const commentBlock = (
-    <div className='comments'>
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map(comment => (
-          <li key={comment.id}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  )
-
   return (
     <div className='container'>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
+      <h1>My App</h1>
     </div>
   )
 }
