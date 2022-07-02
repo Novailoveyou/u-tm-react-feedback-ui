0f63272b93280cd7eb39f429f1dfd128480f4555
novailoveyou <novailoveyoux@gmail.com>
2022-07-02 22:27:34 +0300

    dynamic values & lists in JDX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
 const App = () => {
-  return React.createElement(
-    'div',
-    { className: 'container' },
-    React.createElement('h1', {}, 'My App')
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {
+      id: 1,
+      text: 'Comment one'
+    },
+    {
+      id: 2,
+      text: 'Comment two'
+    },
+    {
+      id: 3,
+      text: 'Comment three'
+    }
+  ]
+
+  return (
+    <div className='container'>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map(comment => (
+            <li key={comment.id}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
