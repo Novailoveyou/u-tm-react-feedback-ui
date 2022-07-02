69a0954293074676c1a8de414a96d5b5752db3d0
novailoveyou <novailoveyoux@gmail.com>
2022-07-03 02:27:57 +0300

    Card component & Conditional styles

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
 const FeedbaclList = ({ feedback }) => {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>
   }
-  console.log(feedback)
   return (
     <div className='feedback-list'>
       {feedback.map(item => (
         <FeedbackItem key={item.id} item={item} />
       ))}
     </div>
+  )
+}
+
+FeedbaclList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
   )
 }
 
