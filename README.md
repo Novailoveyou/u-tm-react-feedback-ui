755a256e8f8b83497ab6da368c2be3f402120f89
novailoveyou <novailoveyoux@gmail.com>
2022-07-03 01:51:10 +0300

    cleaned up FeedbackItem

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ This is me following Brad Traversy's
 [udemy course](https://www.udemy.com/course/react-front-to-back-2022)
 
 Year 2022
+
+## Things I learned
+
+- For the useState hook you can pass in a function to setState function that
+  takes in `prev` prop to access previous state
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,17 +4,10 @@ const FeedbackItem = () => {
   const [rating, setRating] = useState(7)
   const [text, setText] = useState('This is an example of a feedback item')
 
-  const handleClick = () => {
-    setRating(prev => {
-      return prev + 1
-    })
-  }
-
   return (
     <div className='card'>
       <div className='num-display'>{rating}</div>
       <div className='text-display'>{text}</div>
-      <button onClick={handleClick}>Click</button>
     </div>
   )
 }
