b6c4aaecd945174fe7ef47287cc36939bb9e0eaa
novailoveyou <novailoveyoux@gmail.com>
2022-07-08 10:35:42 +0300

    FeedbackStats Component & Reactivity

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+const FeedbackStats = ({ feedback }) => {
+  // Calculate ratings avg
+  let average = (
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating
+    }, 0) / feedback.length
+  )
+    .toFixed(1)
+    .replace(/[.,]0$/, '')
+
+  return (
+    <div className='feedback-stats'>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  )
+}
+
+export default FeedbackStats
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired
+}
