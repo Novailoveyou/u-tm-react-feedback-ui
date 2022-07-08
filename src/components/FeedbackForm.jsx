61454a105501effe1d946c6a954e4a24ba8718b0
novailoveyou <novailoveyoux@gmail.com>
2022-07-08 11:04:32 +0300

    custom Button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 const FeedbackForm = () => {
   const [text, setText] = useState('')
@@ -20,7 +21,7 @@ const FeedbackForm = () => {
             placeholder='Write a review'
             value={text}
           />
-          <button type='submit'>Send</button>
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
