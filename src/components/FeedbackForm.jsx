7de0b45de678407fec65dbc2c86d9d94ad124d0e
novailoveyou <novailoveyoux@gmail.com>
2022-07-08 11:36:36 +0300

    Add Feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import RatingSelect from './RatingSelect'
 import Card from './shared/Card'
 import Button from './shared/Button'
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ handleAdd }) => {
   const [text, setText] = useState('')
   const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
@@ -24,9 +24,28 @@ const FeedbackForm = () => {
     setText(e.target.value)
   }
 
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if (text.trim().length < 10) {
+      return
+    }
+
+    const newFeedback = {
+      text,
+      rating
+    }
+
+    handleAdd(newFeedback)
+
+    setText('')
+    setRating(10)
+    setBtnDisabled(true)
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
         <RatingSelect select={rating => setRating(rating)} />
         <div className='input-group'>
