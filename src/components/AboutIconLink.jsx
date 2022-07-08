56c4e0c51b2131ed228b31d247abfa5a512325ab
novailoveyou <novailoveyoux@gmail.com>
2022-07-08 12:18:32 +0300

    cleaned up AboutIconLink

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const AboutIconLink = () => {
   return (
     <div className='about-link'>
-      <Link to={{ pathname: '/about', search: '?sort=name', hash: '#hello' }}>
+      <Link to='/about'>
         <FaQuestion size={30} />
       </Link>
     </div>
