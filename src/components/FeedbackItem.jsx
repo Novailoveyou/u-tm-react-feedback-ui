ace7d85b463d5a4b2cb207eae42081c814cfaa81
novailoveyou <novailoveyoux@gmail.com>
2022-07-09 17:45:22 +0300

    edit feedback event

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,20 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import { useContext } from 'react'
 import Card from './shared/Card'
 import FeedbackContext from '../context/FeedbackContext'
 
 const FeedbackItem = ({ item }) => {
-  const { deleteFeedback } = useContext(FeedbackContext)
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
 
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
       <button className='close' onClick={() => deleteFeedback(item.id)}>
         <FaTimes color='purple' />
+      </button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' />
       </button>
       <div className='text-display'>{item.text}</div>
     </Card>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,6 +22,11 @@ export const FeedbackProvider = ({ children }) => {
     }
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
   const addFeedback = newFeedback => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
@@ -33,12 +38,20 @@ export const FeedbackProvider = ({ children }) => {
     }
   }
 
+  const editFeedback = item => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
         addFeedback,
-        deleteFeedback
+        deleteFeedback,
+        editFeedback
       }}>
       {children}
     </FeedbackContext.Provider>
