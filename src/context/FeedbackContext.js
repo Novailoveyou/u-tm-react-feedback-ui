50ed5c499b34e25073c180a039d88910c3aa3556
novailoveyou <novailoveyoux@gmail.com>
2022-07-10 14:38:38 +0300

    fetch data from json-server backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,31 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 10
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 9
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 7
-    }
-  ])
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const res = await fetch(
+      'http://localhost:5000/feedback?_sort=id&_order=desc'
+    )
+    const data = await res.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   const addFeedback = newFeedback => {
     newFeedback.id = uuidv4()
@@ -56,6 +56,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         addFeedback,
         deleteFeedback,
         editFeedback,
